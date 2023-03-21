08e648a9a2dea27dba2bd4d82742822adc3e9bfc
Jessica Dupont <jessica.dupont1990@gmail.com>
2023-03-21 11:23:17 +0100

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 import { ENV } from './shared/env';
+import { SwaggerConfig } from './shared/swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, SwaggerConfig);
+  SwaggerModule.setup('', app, document);
+
+  console.log("http://localhost:"+ENV.port);
+  console.log("Documentation swagger.json : http://localhost:"+ENV.port+"/-json");
+
   await app.listen(ENV.port);
 }
 bootstrap();
---
+++ src/shared/swagger/swagger.config.ts
@@ -0,0 +1,21 @@
+import { DocumentBuilder } from "@nestjs/swagger";
+
+export const SwaggerConfig = new DocumentBuilder()
+    .setTitle('DoricAPI')
+    .setDescription('API fournissant les informations sur Dorica RPG')
+    .setVersion('1.0')
+    .setContact(
+        "Jessica Dupont",
+        "http://www.api.dorica.miss-ica.be",
+        "contact@jessicadupont.net"
+    )
+    //.addTag('Clients', "Liste des FRONT utilisant l'API")
+    .addTag('Users', "Personnes utilisant l'API")
+    .addBearerAuth({
+        type: 'http',
+        name: 'Bearer',
+        bearerFormat: "Bearer",
+        in: 'Header',
+        scheme: "Bearer"
+    }, "dorica_access")
+    .build();
